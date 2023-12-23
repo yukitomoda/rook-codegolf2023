021ba0d477685cfc026a964ca4018f9ecc2b71ba
yukitomoda <tomoda@rook-jp.com>
2023-12-23 14:38:49 +0900

    feat: navbarをbulmaの実装に置き換え

--- src/index.js
+++ src/index.js
@@ -3,4 +3,20 @@ import Pace from 'pace-js';
 import 'destyle.css';
 import './sass/index.scss';
 
-Pace.start();
+window.addEventListener('DOMContentLoaded', () => {
+  Pace.start();
+  navbar();
+});
+
+function navbar() {
+  const burgers = document.querySelectorAll('.navbar-burger');
+  burgers.forEach((burger) => {
+    const targetId = burger.dataset.target;
+    const target = document.getElementById(targetId);
+
+    burger.addEventListener('click', () => {
+      burger.classList.toggle('is-active');
+      target.classList.toggle('is-active');
+    });
+  });
+}
