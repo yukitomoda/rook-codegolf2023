337c1fbae76d4be5c85cc60892a8b91f784b1691
yukitomoda <tomoda@rook-jp.com>
2023-12-24 18:47:31 +0900

    feat: コードゴルフ例用のスタイルを作成

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import './sass/index.scss';
 window.addEventListener('DOMContentLoaded', () => {
   Pace.start();
   navbar();
+  tabs();
 });
 
 function navbar() {
@@ -19,4 +20,43 @@ function navbar() {
       target.classList.toggle('is-active');
     });
   });
+}
+
+function tabs() {
+  const tabGroups = document.querySelectorAll('.tabs');
+  tabGroups.forEach((group) => {
+    const tabs = group.querySelectorAll('li');
+    const groupId = group.dataset.tabGroup;
+
+    tabs.forEach((tab) => {
+      const tabId = tab.dataset.tabId;
+      tab.addEventListener('click', () => {
+        update(groupId, tabId);
+      });
+
+      if (tab.classList.contains('is-active')) {
+        update(groupId, tabId);
+      }
+    });
+  });
+
+  function update(groupId, tabId) {
+    const tabs = document.querySelectorAll(`.tabs[data-tab-group=${groupId}] li`);
+    tabs.forEach((tab) => {
+      if (tabId === tab.dataset.tabId) {
+        tab.classList.add('is-active');
+      } else {
+        tab.classList.remove('is-active');
+      }
+    });
+
+    const panes = document.querySelectorAll(`.tab-panes[data-tab-group=${groupId}] .tab-pane`);
+    panes.forEach((pane) => {
+      if (tabId === pane.dataset.tabId) {
+        pane.classList.add('is-active');
+      } else {
+        pane.classList.remove('is-active');
+      }
+    });
+  }
 }
